cc0c047000a27bf9ed78d4c94706f4697612b94b
Ishara Lakshan <ishara@tuta.io>
2026-09-16 10:39:23 +0530

    feat: initialize desktop window manager for fullscreen playback

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    await windowManager.ensureInitialized();
+  }
+
   runApp(const PikoraApp());
 }
