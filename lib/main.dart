fb8e5c5b00a2f9d441c08407b26db420fd032236
Ishara Lakshan <ishara@tuta.io>
2026-09-17 21:19:52 +0530

    feat: initialize optional Orvix account backend

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:media_kit/media_kit.dart';
 import 'package:window_manager/window_manager.dart';
 
 import 'app.dart';
+import 'services/account_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MediaKit.ensureInitialized();
+  await AccountService.initializeBackend();
 
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     await windowManager.ensureInitialized();
