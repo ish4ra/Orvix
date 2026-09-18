67e17890b9129e7bf4cb4a166d3ef6d6dac06f24
Ishara Lakshan <ishara@tuta.io>
2026-09-19 01:23:09 +0530

    chore: update alpha.05 about version

--- lib/app.dart
+++ lib/app.dart
@@ -513,7 +513,7 @@ class _AboutScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Orvix v0.7.4-alpha.04',
+                'Orvix v0.7.4-alpha.05',
                 style: Theme.of(context)
                     .textTheme
                     .headlineMedium
