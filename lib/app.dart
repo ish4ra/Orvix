26c306e1025c84b2ff1c563193c3dcbd15b700a8
Ishara Lakshan <ishara@tuta.io>
2026-09-19 01:55:20 +0530

    chore: show alpha.07 in About

--- lib/app.dart
+++ lib/app.dart
@@ -513,7 +513,7 @@ class _AboutScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Orvix v0.7.4-alpha.06',
+                'Orvix v0.7.4-alpha.07',
                 style: Theme.of(context)
                     .textTheme
                     .headlineMedium
