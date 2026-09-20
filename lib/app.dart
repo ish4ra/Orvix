1f996aae6b93b5fcd5eccfb004f40880994d2fb8
Ishara Lakshan <ishara@tuta.io>
2026-09-20 06:20:31 +0530

    fix(about): show current beta.6 version

--- lib/app.dart
+++ lib/app.dart
@@ -533,7 +533,7 @@ class _AboutScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                'Orvix v0.7.4-alpha.09',
+                'Orvix v0.7.5-beta.6',
                 style: Theme.of(context)
                     .textTheme
                     .headlineMedium
