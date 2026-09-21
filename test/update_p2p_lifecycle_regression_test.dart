f24181479633403ba0a27579c75e5d9b001fa96a
Ishara Lakshan <ishara@tuta.io>
2026-09-21 14:50:40 +0530

    test(update): track v0.7.6-beta.1 runtime

--- test/update_p2p_lifecycle_regression_test.dart
+++ test/update_p2p_lifecycle_regression_test.dart
@@ -28,7 +28,7 @@ void main() {
     expect(app, contains('AppLifecycleState.detached'));
     expect(app, contains('OrvixUpdateGate'));
 
-    expect(update, contains("currentVersion = '0.7.5'"));
+    expect(update, contains("currentVersion = '0.7.6-beta.1'"));
     expect(update, contains("name.contains('Windows-x64')"));
     expect(update, contains('Android-TV.apk'));
     expect(update, contains('Android-Mobile.apk'));
