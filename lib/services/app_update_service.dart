f6293269bc6dc0a375803c241a983b22ae5a4637
Ishara Lakshan <ishara@tuta.io>
2026-09-22 02:19:18 +0530

    fix(update): report beta.3 runtime version

--- lib/services/app_update_service.dart
+++ lib/services/app_update_service.dart
@@ -45,7 +45,7 @@ enum AppUpdateInstallResult {
 class AppUpdateService {
   AppUpdateService({http.Client? client}) : _client = client ?? http.Client();
 
-  static const currentVersion = '0.7.6-beta.2';
+  static const currentVersion = '0.7.6-beta.3';
   static const _releasesUrl =
       'https://api.github.com/repos/ish4ra/Orvix/releases?per_page=12';
   static const MethodChannel _androidUpdateChannel =
