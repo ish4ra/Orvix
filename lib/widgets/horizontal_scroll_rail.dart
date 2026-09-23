aaa829ab4cff434576442c4c789fabe700a9dfd1
Ishara Lakshan <ishara@tuta.io>
2026-09-23 09:13:11 +0530

    chore(ui): use current horizontal rail cache API

--- lib/widgets/horizontal_scroll_rail.dart
+++ lib/widgets/horizontal_scroll_rail.dart
@@ -114,7 +114,7 @@ class _HorizontalScrollRailState extends State<HorizontalScrollRail> {
           scrollDirection: Axis.horizontal,
           physics: const ClampingScrollPhysics(),
           padding: widget.padding,
-          cacheExtent: 1800,
+          scrollCacheExtent: 1800,
           itemCount: widget.itemCount,
           separatorBuilder: (_, __) => SizedBox(width: widget.separatorWidth),
           itemBuilder: widget.itemBuilder,
