c01c02aa98545bc3ed0034adb29c79b622f2d79e
Ishara Lakshan <ishara@tuta.io>
2026-09-23 05:49:01 +0530

    test(ai): align complete translation batching regression

--- test/subtitle_render_pipeline_test.dart
+++ test/subtitle_render_pipeline_test.dart
@@ -8,9 +8,10 @@ void main() {
         File('lib/services/ai_sinhala_subtitle_service.dart').readAsStringSync();
 
     expect(service, contains('static Future<void> _translateEntireSubtitle('));
-    expect(service, contains('const batchSize = 60;'));
-    expect(service, contains('const parallelBatches = 3;'));
+    expect(service, contains('const batchSize = 28;'));
+    expect(service, contains('const parallelBatches = 2;'));
     expect(service, contains('await Future.wait<void>'));
+    expect(service, contains('_translateIndicesResilient('));
     expect(
       service,
       contains('prepared.translatedCount != prepared.cues.length'),
