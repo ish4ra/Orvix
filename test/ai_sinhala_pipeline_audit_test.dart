3d756d6e3d39318158a5628302da6f17edb02ab1
Ishara Lakshan <ishara@tuta.io>
2026-09-23 18:06:39 +0530

    test: lock faster full-file translation waves

--- test/ai_sinhala_pipeline_audit_test.dart
+++ test/ai_sinhala_pipeline_audit_test.dart
@@ -60,8 +60,8 @@ void main() {
     final end = service.indexOf('static Future<AiPreparedSubtitle?> prepareBuffered', start);
     final translate = service.substring(start, end);
 
-    expect(translate, contains('const batchSize = 28;'));
-    expect(translate, contains('const parallelBatches = 2;'));
+    expect(translate, contains('const batchSize = 96;'));
+    expect(translate, contains('const parallelBatches = 3;'));
     expect(translate, contains('await Future.wait<void>'));
     expect(translate, contains('_translateIndicesResilient('));
   });
