1ac0750417af96bc8bdb50622355923b686eed4b
Ishara Lakshan <ishara@tuta.io>
2026-09-24 16:00:51 +0530

    chore: sync updater version with beta.34

--- lib/services/app_update_service.dart
+++ lib/services/app_update_service.dart
@@ -46,7 +46,7 @@ enum AppUpdateInstallResult {
 class AppUpdateService {
   AppUpdateService({http.Client? client}) : _client = client ?? http.Client();
 
-  static const currentVersion = '0.7.6-beta.33';
+  static const currentVersion = '0.7.6-beta.34';
   static const _releasesBaseUrl =
       'https://api.github.com/repos/ish4ra/Orvix/releases';
   static const _releaseAssetFetchAttempts = 5;
