ccbe6d7cd2ae68131cc5e79830d0b95f84814d3c
Ishara Lakshan <ishara@tuta.io>
2026-09-25 08:27:36 +0530

    test: cover one-word dialogue and ASS drawing rejection

--- test/native_subtitle_event_parser_test.dart
+++ test/native_subtitle_event_parser_test.dart
@@ -26,12 +26,15 @@ Dialogue: 0,0:00:11.20,0:00:15.00,Default,,0,0,0,,Second line, with a comma
 Dialogue: 0,0:00:01.00,0:00:03.00,Default,,0,0,0,,Hello there\NHow are you?
 Dialogue: 0,0:00:01.00,0:00:03.00,Default,,0,0,0,,Hello there\NHow are you?
 Dialogue: 0,0:00:04.00,0:00:06.00,Default,,0,0,0,,こんにちは 世界
+Dialogue: 0,0:00:07.00,0:00:08.00,Default,,0,0,0,,No.
+Dialogue: 0,0:00:09.00,0:00:10.00,Default,,0,0,0,,{\p1}m 0 0 l 10 10{\p0}
 Dialogue: 0,0:99:00.00,0:99:02.00,Default,,0,0,0,,Invalid timing line
 ''';
 
     final events = NativeSubtitleEventParser.parseAssFull(raw);
 
-    expect(events, hasLength(1));
-    expect(events.single.text, 'Hello there\nHow are you?');
+    expect(events, hasLength(2));
+    expect(events[0].text, 'Hello there\nHow are you?');
+    expect(events[1].text, 'No.');
   });
 }
